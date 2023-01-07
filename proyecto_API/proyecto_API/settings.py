dc576541bb29648acbcaeebc022a6a9bbfba64f2
Aceballos15 <ceballoscardonaalexander@gmail.com>
2023-01-07 15:03:12 -0500

    agregado readem and ignore

--- proyecto_API/proyecto_API/settings.py
+++ proyecto_API/proyecto_API/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'django-api', 
         'USER': 'postgres', 
-        'PASSWORD': '#',
+        'PASSWORD': 'aceballos',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
